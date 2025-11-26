c0cd502666961a195b2c2902682870c62b37bfb4
Agung Dwi Sahputra <agungdwisahputra@gmail.com>
2025-11-27 05:03:02 +0700

    feat(jeep-trip): configure midtrans production support and fix merchant id typo
    Add conditional loading of Midtrans Snap.js script based on production environment,
    include merchant_id in config, and correct typo in .env.example.

--- resources/views/landing/jeep-trip/checkout.blade.php
+++ resources/views/landing/jeep-trip/checkout.blade.php
@@ -253,8 +253,12 @@
 
     @push('js')
         <!-- Midtrans Snap.js -->
-        <script src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key="{{ config('midtrans.client_key') }}"></script>
-
+        @if(config('midtrans.is_production'))
+            <script src="https://app.midtrans.com/snap/snap.js" data-client-key="{{ config('midtrans.client_key') }}"></script>
+        @else
+            <script src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key="{{ config('midtrans.client_key') }}"></script>
+        @endif
+        
         <script>
             let snapToken = null;
 
